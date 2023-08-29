36910cb5be4a928dc1ec5ebb640bbeb0d2d2d991
Haofan Wang <haofanwang.ai@gmail.com>
2023-08-30 03:11:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,12 @@ apt-get update
 apt-get install tmux
 apt-get install libgl1
 
-export https_proxy=10.7.4.2:3128
-
 pip install torch==1.13.1 torchvision==0.14.1 --extra-index-url https://download.pytorch.org/whl/cu116
 
 pip install diffusers==0.20.1 transformers==4.25.1 xformers==0.0.16 imageio==2.31.2 einops omegaconf safetensors accelerate decord compel
 
 # lama
 pip install pyyaml easydict scikit-image scikit-learn joblib matplotlib pandas tabulate webdataset albumentations==0.5.2 hydra-core==1.1.0
-
 
 pip install numpy==1.22.3
 pip install kornia==0.5.0 
